05d58273f873aa25168a5571dc9741a1c2750e97
IFmund <ifmund.dev@gmail.com>
2025-03-13 13:39:54 +0300

    little refactor

--- bin/toggle-polybar.sh
+++ bin/toggle-polybar.sh
@@ -6,5 +6,5 @@ if test $status -eq 0
 then
   pkill polybar && bspc config -m focused top_padding 0
 else
-  $HOME/bin/launch-polybar.sh && bspc config -m focused top_padding 25
+  $HOME/bin/launch-polybar.sh && bspc config -m focused top_padding 35
 fi
